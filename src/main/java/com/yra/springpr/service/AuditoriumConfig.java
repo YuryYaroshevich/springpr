0f0ae7f3e7b0e0df25cff9a3872980f500420413
Yury Yaroshevich <Yury_Yaroshevich@epam.com>
2016-02-07 12:24:47 +0300

    finished with configuring beans

--- src/main/java/com/yra/springpr/service/AuditoriumConfig.java
+++ src/main/java/com/yra/springpr/service/AuditoriumConfig.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
@@ -17,16 +18,15 @@ import com.yra.springpr.model.Auditorium;
 import com.yra.springpr.util.StringToSet;
 
 @Configuration
-public class AuditoriumConfig {
-    private static final int AUDITORIUMS_NUM = 3;
-    
-    //@Autowired
+public class AuditoriumConfig {    
+    @Autowired
     private Converter<String, Set<Integer>> stringToSetConverter = new StringToSet();
     
     @Bean(name = "auditoriums")
     public Map<Integer, Auditorium> createAuditoriums() throws IOException {
         Resource resource = new ClassPathResource("config/auditoriums.properties");
         Properties auditoriumProps = PropertiesLoaderUtils.loadProperties(resource);
+        final int AUDITORIUMS_NUM = Integer.valueOf(auditoriumProps.getProperty("auditorums.num"));
         Map<Integer, Auditorium> auditoriums = new HashMap<>();
         for (int i = 1; i <= AUDITORIUMS_NUM; i++) {
             Auditorium auditorium = new Auditorium(
